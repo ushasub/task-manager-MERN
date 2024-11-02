94e40891ec61066b513c071e715355003fc5cebe
ushasub <usha.s@chanakyauniversity.edu.in>
2024-11-02 19:05:56 +0530

    Update server.js and package.json for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const connectDB = require('./connect/database');
@@ -22,9 +23,18 @@ app.use((err, req, res, next) => {
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tasks', require('./routes/taskRoutes'))
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send('API is running')
-})
+})*/
+
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
